743ea47258c946d2e759807205e1378495a44bbb
Daniel Mauricio Padilla González <167046547+mahurisio@users.noreply.github.com>
2026-05-15 15:22:42 -0500

    Conectar rutas iniciales

---
+++ src/routes/__init__.py
@@ -0,0 +1,5 @@
+from src.routes.evaluations import router as evaluations_router
+from src.routes.games import router as games_router
+from src.routes.players import router as players_router
+
+__all__ = ["evaluations_router", "games_router", "players_router"]
